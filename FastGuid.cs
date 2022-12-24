2aa6abff8ac4816655593c9f06e07a892ee07cea
Stan Drapkin <sdrapkin@sdprime.com>
2022-12-24 13:08:23 -0500

    Performance optimizations v1.0.4

--- FastGuid.cs
+++ FastGuid.cs
@@ -14,34 +14,13 @@ namespace SecurityDriven
 		const int GUIDS_PER_THREAD = 256; //keep it power-of-2
 		const int GUID_SIZE_IN_BYTES = 16;
 
-		[ThreadStatic] static Container ts_data;
-
-		static Container LocalContainer
+		struct Container
 		{
-			[MethodImpl(MethodImplOptions.AggressiveInlining)]
-			get => ts_data ??= new();
+			public Guid[] _guids;
+			public int _idx;
 		}
-		sealed class Container
-		{
-			Guid[] _guids = GC.AllocateUninitializedArray<Guid>(GUIDS_PER_THREAD);
-			int _idx;
 
-			[MethodImpl(MethodImplOptions.AggressiveInlining)]
-			public Guid NewGuid()
-			{
-				ref var guid0 = ref MemoryMarshal.GetArrayDataReference(_guids);
-				int idx = _idx++ & (GUIDS_PER_THREAD - 1);
-				if (idx == 0)
-				{
-					RandomNumberGenerator.Fill(
-						MemoryMarshal.CreateSpan<byte>(ref Unsafe.As<Guid, byte>(ref guid0), GUIDS_PER_THREAD * GUID_SIZE_IN_BYTES));
-				}
-
-				var guid = Unsafe.Add(ref guid0, idx);
-				Unsafe.Add(ref guid0, idx) = default; // prevents Guid leakage
-				return guid;
-			}//NextGuid()
-		}//class Container
+		[ThreadStatic] static Container ts_data;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Guid"/> structure.
@@ -49,6 +28,19 @@ namespace SecurityDriven
 		/// <returns>A new <see cref="Guid"/> struct.</returns>
 		/// <remarks>Faster alternative to <see cref="Guid.NewGuid"/>.</remarks>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Guid NewGuid() => LocalContainer.NewGuid();
+		public static Guid NewGuid()
+		{
+			ref var guid0 = ref MemoryMarshal.GetArrayDataReference(ts_data._guids ??= GC.AllocateUninitializedArray<Guid>(GUIDS_PER_THREAD));
+			int idx = ts_data._idx++ & (GUIDS_PER_THREAD - 1);
+			if (idx == 0)
+			{
+				RandomNumberGenerator.Fill(
+					MemoryMarshal.CreateSpan<byte>(ref Unsafe.As<Guid, byte>(ref guid0), GUIDS_PER_THREAD * GUID_SIZE_IN_BYTES));
+			}
+
+			var guid = Unsafe.Add(ref guid0, idx);
+			Unsafe.Add(ref guid0, idx) = default; // prevents Guid leakage
+			return guid;
+		}//NewGuid()
 	}//class FastGuid
 }//ns
