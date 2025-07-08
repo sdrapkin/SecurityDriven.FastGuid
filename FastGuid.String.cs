1aa1771482c8ed7b4024d4cfa21129646347f608
Stan Drapkin <sdrapkin@sdprime.com>
2025-07-08 10:36:46 -0400

    Add Text32c (Crockford Base32)

--- FastGuid.String.cs
+++ FastGuid.String.cs
@@ -5,19 +5,20 @@ namespace SecurityDriven
 {
 	public static partial class FastGuid
 	{
-		// Copyright (c) 2025 Stan Drapkin
-		// LICENSE: https://github.com/sdrapkin/SecurityDriven.FastGuid
+		// Copyright (c) 2025 Stan Drapkin  
+		// LICENSE: https://github.com/sdrapkin/SecurityDriven.FastGuid  
 
-		/// <summary>Generates random text strings using Base16/Base32/Base64/Base64Url alphabets.</summary>
+		/// <summary>Generates random text strings using Base16/Base32/Base64/Base64Url alphabets.</summary>  
 		public static class StringGen
 		{
-			// RFC 4648 alphabets
+			// RFC 4648 alphabets  
 			const string Base16 = "0123456789ABCDEF";
 			const string Base32 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+			const string Base32c = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"; // Crockford Base32
 			const string Base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 			const string Base64Url = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
-			// Repeat the alphabets to 256 bytes
+			// Repeat the alphabets to 256 bytes  
 			const string Base16_256 =
 				Base16 + Base16 + Base16 + Base16 +
 				Base16 + Base16 + Base16 + Base16 +
@@ -27,6 +28,10 @@ namespace SecurityDriven
 			const string Base32_256 =
 				Base32 + Base32 + Base32 + Base32 +
 				Base32 + Base32 + Base32 + Base32;
+
+			const string Base32c_256 =
+				Base32c + Base32c + Base32c + Base32c +
+				Base32c + Base32c + Base32c + Base32c;
 
 			const string Base64_256 = Base64 + Base64 + Base64 + Base64;
 			const string Base64Url_256 = Base64Url + Base64Url + Base64Url + Base64Url;
@@ -42,17 +47,20 @@ namespace SecurityDriven
 							charSpan[i] = _alphabet256[byteSpan[i]];
 					});
 
-			/// <summary>Generates a random text string using Base16 alphabet.</summary>
+			/// <summary>Generates a random text string using Base16 alphabet.</summary>  
 			public static string Text16(int length) => TextAlphabet256(length, Base16_256);
 
-			/// <summary>Generates a random text string using Base32 alphabet.</summary>
+			/// <summary>Generates a random text string using Base32 alphabet.</summary>  
 			public static string Text32(int length) => TextAlphabet256(length, Base32_256);
 
-			/// <summary>Generates a random text string using Base64 alphabet.</summary>
+			/// <summary>Generates a random text string using Base32 Crockford alphabet.</summary>  
+			public static string Text32c(int length) => TextAlphabet256(length, Base32c_256);
+
+			/// <summary>Generates a random text string using Base64 alphabet.</summary>  
 			public static string Text64(int length) => TextAlphabet256(length, Base64_256);
 
-			/// <summary>Generates a random text string using Base64Url alphabet.</summary>
+			/// <summary>Generates a random text string using Base64Url alphabet.</summary>  
 			public static string Text64Url(int length) => TextAlphabet256(length, Base64Url_256);
-		}// static class StringGen
-	}//class FastGuid
+		}// static class StringGen  
+	}//class FastGuid  
 }//ns
